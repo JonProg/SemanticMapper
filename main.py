83295511b0d027bcb4585e98219c41d9b7ebad87
jon_prog <jonashenri.aquino@gmail.com>
2023-08-22 07:39:00 -0300

    add sujeitos of variable frase

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ Gerard Piqué. A partida foi marcada por lances emocionantes, belos gols e uma
 intensa rivalidade entre as equipes. No final, o Real Madrid saiu vitorioso, com um 
 placar de 3 a 2, em um jogo que ficará marcado na história do futebol.""")
 
+frase = nlp_pt("O cachorro latiu durante a noite.")
+
 #_____________________________________________
 #Entidades no texto |
 entidades_text = list(doc_pt.ents)
 print(f'Entidades do texto -> {entidades_text}')
+
 
 #_____________________________________________
 #Classe gramaticais |
@@ -43,15 +46,22 @@ VERB: verb
 X: other
 
 """
-
+#_____________________________________________
+#Detalhes das entidades |
 entidades_detail = [(entidade, entidade.label_) for entidade in doc_pt.ents]
 print(f'Detalhamento das entidades -> {etiquetas_class}')
 
 
 #_____________________________________________
+#Sujeito da frase |
+sujeito = [suj for suj in frase if suj.dep_ == 'nsubj']
+print(f'Sujeitos -> {sujeito}')
+
+
+#_____________________________________________
 #Lematização |
 lemmas = [token.lemma_ for token in doc_pt if token.pos_ == 'VERB']
-print(lemmas)
+print(f'Lematização dos verbos -> {lemmas}')
 
 
 
